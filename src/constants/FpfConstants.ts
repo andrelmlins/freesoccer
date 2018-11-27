5f61d310078bfbcc73bc348d85e34a6142f2da42
André Lins <andrelucas01@hotmail.com>
2018-11-27 09:34:53 -0300

    Ajustment error in FpfConstants

--- src/constants/FpfConstants.ts
+++ src/constants/FpfConstants.ts
@@ -23,7 +23,7 @@ export default class FpfConstants {
             name:"Segunda Liga - Ledman LigaPro",
             years:["2009","2010","2011","2012","2013","2014","2015","2016","2017","2018"],
             aux:{
-                urls:["ligavitalis","ligaorangina","ligaorangina","segundaliga","ligazonsagres","liga2cabovisao","segundaliga","ledmanligapro","ledmanligapro","ledmanligapro"]
+                urls:["ligavitalis","ligaorangina","ligaorangina","segundaliga","liga2cabovisao","segundaliga","ledmanligapro","ledmanligapro","ledmanligapro","ledmanligapro"]
             }
         }
     ];
