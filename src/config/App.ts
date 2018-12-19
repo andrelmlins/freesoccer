c086be0ea6245e24bb2958269ffb686bb9a4b600
André Lucas Machado Lins <andre.lins@softplan.com.br>
2018-12-19 16:52:16 -0200

    Refresh mongoose

--- src/config/App.ts
+++ src/config/App.ts
@@ -1,46 +1,49 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
-import * as mongoose from 'mongoose';
-import * as morgan from 'morgan';
-import * as cors from 'cors';
+import * as mongoose from "mongoose";
+import * as morgan from "morgan";
+import * as cors from "cors";
 import * as path from "path";
-import * as swaggerUi from 'swagger-ui-express';
-var swaggerDocument = require('../../public/assets/swagger.json');
+import * as swaggerUi from "swagger-ui-express";
+var swaggerDocument = require("../../public/assets/swagger.json");
 
 import { Routes } from "./Routes";
 import CronJobs from "./CronJobs";
 
 class App {
+  public app: express.Application;
+  public routes: Routes;
+  public cronJobs: CronJobs;
 
-    public app: express.Application;
-    public routes: Routes;
-    public cronJobs : CronJobs;
+  constructor() {
+    this.routes = new Routes();
+    this.cronJobs = new CronJobs();
 
-    constructor() {
-        this.routes = new Routes;
-        this.cronJobs = new CronJobs;
+    this.app = express();
+    this.config();
 
-        this.app = express();
-        this.config(); 
+    this.app.use(express.static(path.join(__dirname + "../../../public")));
+    this.app.use(
+      "/docs",
+      swaggerUi.serve,
+      swaggerUi.setup(swaggerDocument, {
+        customCss:
+          "body { background-color: rgb(255, 255, 255) !important } .swagger-ui .topbar { display: none } .swagger-ui .information-container { display: none }"
+      })
+    );
 
-        this.app.use(express.static(path.join(__dirname + '../../../public')));
-        this.app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument,{
-            customCss: 'body { background-color: rgb(255, 255, 255) !important } .swagger-ui .topbar { display: none } .swagger-ui .information-container { display: none }'
-        }));
+    this.routes.routes(this.app);
+    this.cronJobs.crons();
+  }
 
-        this.routes.routes(this.app);
-        this.cronJobs.crons();
-    }
+  private config(): void {
+    mongoose.connect("mongodb://localhost/freesoccer");
 
-    private config(): void{
-        mongoose.connect('mongodb://localhost/freesoccer', { useNewUrlParser: true });
-
-        this.app.use(bodyParser.json({ limit: '50mb' }));
-        this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-        this.app.use(cors());
-        this.app.use(morgan('dev'));
-    }
-
+    this.app.use(bodyParser.json({ limit: "50mb" }));
+    this.app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+    this.app.use(cors());
+    this.app.use(morgan("dev"));
+  }
 }
 
 export default new App().app;
