88ee0674ac56841cce0ac9e6c35c5f86784e0521
André Lucas Machado Lins <andre.lins@softplan.com.br>
2018-12-20 11:22:14 -0200

    https

--- website/src/services/freesoccer.js
+++ website/src/services/freesoccer.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "http://www.apifreesoccer.com/api";
+const URL = "https://www.apifreesoccer.com/api";
 
 export default class FreeSoccerService {
   static async login(user) {
