d8f1f6f3c2832b978f8409219310557ffed8802e
André Lucas Machado Lins <andre.lins@softplan.com.br>
2018-12-20 11:27:09 -0200

    Documentation

--- website/src/screens/DocumentationScreen.js
+++ website/src/screens/DocumentationScreen.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { withRouter } from 'react-router-dom';
-import Typography from '@material-ui/core/Typography';
+import { withRouter } from "react-router-dom";
+import Typography from "@material-ui/core/Typography";
 
 class DocumentaionScreen extends Component {
-    render() {
-        return (
-            <div>
-                <Typography variant="h2" color="inherit" noWrap>
-                    Documentation
-                </Typography>
-                <br /><br />
-                <iframe src="http://www.apifreesoccer.com/docs/" title="Documentation" width="100%" height="2000" frameborder="0" scrolling="no"></iframe>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Typography variant="h2" color="inherit" noWrap>
+          Documentation
+        </Typography>
+        <br />
+        <br />
+        <iframe src="https://www.apifreesoccer.com/docs/" title="Documentation" width="100%" height="2000" frameborder="0" scrolling="no" />
+      </div>
+    );
+  }
 }
 
 export default withRouter(DocumentaionScreen);
