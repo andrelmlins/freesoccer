3e796e84a7493bdaab203d4010d9582bb480b540
André Lins <andrelucas01@hotmail.com>
2019-10-13 09:00:15 -0300

    Inserting types

--- src/graphql/types.ts
+++ src/graphql/types.ts
@@ -3,10 +3,25 @@ const { gql } = require('apollo-server');
 const types = gql`
   type Competition {
     name: String
+    code: String
+    type: String
+    country: String
+    federation: String
   }
 
   type Round {
-    name: String
+    number: String
+    goals: Int
+    goalsHome: Int
+    goalsGuest: Int
+    hash: String
+    url: String
+  }
+
+  type Match {
+    date: String
+    stadium: String
+    location: String
   }
 
   type Query {
