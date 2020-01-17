aaa3c896479e4eb6bbf71eb181fbdf3106b20bc6
André Lins <andrelucas01@hotmail.com>
2020-01-17 09:17:59 -0300

    Removing consoles

--- src/graphql/resolvers/CompetitionResolver.ts
+++ src/graphql/resolvers/CompetitionResolver.ts
@@ -4,12 +4,10 @@ export default class CompetitionResolver {
   private competitionRepository: CompetitionRepository;
 
   constructor() {
-    console.log(new CompetitionRepository());
     this.competitionRepository = new CompetitionRepository();
   }
 
   private async get() {
-    console.log(this.competitionRepository);
     return await this.competitionRepository.all({});
   }
 
